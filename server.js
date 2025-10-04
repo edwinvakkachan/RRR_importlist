de05b9339cc993cbf88a1f382621082914175857
edwin vakkachan <edwinvakkachan@gmail.com>
2025-10-04 14:20:27 +0530

    error

--- server.js
+++ server.js
@@ -342,7 +342,7 @@ app.post('/api/clearlogs', requireBasicAuth, (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
